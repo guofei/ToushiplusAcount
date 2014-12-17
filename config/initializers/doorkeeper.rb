14ad17b0c2e7a3454d5d8ba378affbff848b803a
guofei <guofei1986@gmail.com>
2014-12-18 00:12:26 +0900

    reset doorkeeper config

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -18,6 +18,9 @@ Doorkeeper.configure do
   #   # Example implementation:
   #   Admin.find_by_id(session[:admin_id]) || redirect_to(new_admin_session_url)
   # end
+  admin_authenticator do
+    (current_user && current_user.id == 1) || redirect_to(root_path)
+  end
 
   # Authorization Code expiration time (default 10 minutes).
   # authorization_code_expires_in 10.minutes
@@ -82,6 +85,9 @@ Doorkeeper.configure do
   # skip_authorization do |resource_owner, client|
   #   client.superapp? or resource_owner.admin?
   # end
+  skip_authorization do
+    true
+  end
 
   # WWW-Authenticate Realm (default "Doorkeeper").
   # realm "Doorkeeper"
